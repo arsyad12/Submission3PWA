c97baa5f7bd89b50d1f45706e395c0cf33e43f25
Arsyad Basofil Amri <arsyadbaso123@gmail.com>
2020-10-25 10:14:17 +0700

    workbox and push

--- service-worker.js
+++ service-worker.js
@@ -1,74 +1,69 @@
-const CACHE_NAME = "Football-v1222";
-var urlsToCache = [
-  "/",
-  "/nav.html",
-  "/index.html",
-  "/pages/group.html",
-  "/pages/match.html",
-  "/pages/team.html",
-  "/pages/savedMatch.html",
-  "/pages/savedTeam.html",
-  "/css/materialize.min.css",
-  "/js/materialize.min.js",
-  "/manifest.json",
-  "/js/nav.js",
-  "/js/api.js",
-  "/js/idb.js",
-  "/js/db.js",
-  "/js/script.js",
-  "/icon.png",
-  "/js/pushSW.js",
-  "/push.js",
-  "/package.json",
-  "/package-lock.json"
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-      caches.open(CACHE_NAME).then(function (cache) {
-          return cache.addAll(urlsToCache);
-      })
-  );
-});
+if (workbox){
+    workbox.precaching.precacheAndRoute([
+{ url: '/', revision: '1' },
+{ url: '/nav.html', revision: '1' },
+{ url: '/index.html', revision: '1' },
+{ url: '/pages/group.html', revision: '1' },
+{ url: '/pages/match.html', revision: '1' },
+{ url: '/pages/team.html', revision: '1' },
+{ url: '/pages/savedMatch.html', revision: '1' },
+{ url: '/pages/savedTeam.html', revision: '1' },
+{ url: '/css/materialize.min.css', revision: '1' },
+{ url: '/js/materialize.min.js', revision: '1' },
+{ url: '/manifest.json', revision: '1' },
+{ url: '/js/nav.js', revision: '1' },
+{ url: '/js/api.js', revision: '1' },
+{ url: '/js/idb.js', revision: '1' },
+{ url: '/js/db.js', revision: '1' },
+{ url: '/js/script.js', revision: '1' },
+{ url: '/asset/icon.png', revision: '1' },
+{ url: '/js/pushSW.js', revision: '1' },
+{ url: '/push.js', revision: '1' },
+{ url: '/package.json', revision: '1' },
+{ url: '/package-lock.json', revision: '1' }, 
+]);
 
-self.addEventListener("fetch", function (event) {
-    var base_url = "https://api.football-data.org/";
-    if (event.request.url.indexOf(base_url) > -1) {
-        event.respondWith(
-            caches.open(CACHE_NAME).then(function (cache) {
-                return fetch(event.request).then(function (response) {
-                    cache.put(event.request.url, response.clone());
-                    return response;
-                })
-            })
-        );
-    } else {
-        event.respondWith(
-            caches.match(event.request, {
-                ignoreSearch: true
-            }).then(function (response) {
-                return response || fetch(event.request);
-            })
-        )
-    }
-});
 
-/*event delete cache if CACHE_NAME != CACHE_NAME */
-self.addEventListener("activate", function (event) {
-  event.waitUntil(
-      caches.keys().then(function (cacheNames) {
-          return Promise.all(
-              cacheNames.map(function (cacheName) {
-                  if (cacheName != CACHE_NAME) {
-                      console.log("ServiceWorker: cache " + cacheName + " dihapus");
-                      return caches.delete(cacheName);
-                  }
-              })
-          );
-      })
-  );
-});
+workbox.routing.registerRoute(
+    new RegExp('https://api.football-data.org/v2/'),
+    workbox.strategies.staleWhileRevalidate()
+);
 
+workbox.routing.registerRoute(
+    new RegExp('/pages/'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'pages',
+    })
+);
+
+workbox.routing.registerRoute(
+    /.*(?:png|gif|jpg|jpeg|svg)$/,
+    workbox.strategies.cacheFirst({
+        cacheName: 'image',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200]
+            }),
+            new workbox.expiration.Plugin({
+                maxEntries: 100,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+            }),
+        ]
+    })
+);
+
+workbox.routing.registerRoute(
+    /\.(?:js)$/,
+    workbox.strategies.cacheFirst({
+        cacheName: 'js',
+    })
+);
+
+}else{
+console.log(`Workbox gagal dimuat`);
+}
 
 //Push Notification
 self.addEventListener('push', function (event) {
